a076a307ddce0d9f76b62d567fa037981eca7f96
Shugaeng <resh.spearmint@gmail.com>
2026-08-26 12:18:07 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'gagyebu-cache-v1';
+var APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(APP_SHELL);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  var req = event.request;
+  if (req.method !== 'GET') return;
+
+  // Firebase/외부 API 호출은 캐시하지 않고 네트워크 우선(오프라인 큐잉은 Firestore SDK가 처리)
+  if (req.url.indexOf('firestore.googleapis.com') !== -1 || req.url.indexOf('googleapis.com') !== -1) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(function(cached){
+      var networkFetch = fetch(req).then(function(res){
+        if (res && res.status === 200 && req.url.indexOf('http') === 0) {
+          var resClone = res.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(req, resClone); });
+        }
+        return res;
+      }).catch(function(){ return cached; });
+      return cached || networkFetch;
+    })
+  );
+});
